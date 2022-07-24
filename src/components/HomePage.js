e7dda2e73729739810ea58ab56a8fe5c5853ab6f
Allan <kipropsallan@gmail.com>
2022-07-24 13:57:52 +0300

    Done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,16 @@
 import React, { useEffect, useState } from "react";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 import About from "./About";
 import ArticleList from "./ArticleList";
 
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
+  const { data:posts, isLoaded } = useQuery("http://localhost:4000/posts")
+  console.log({isLoaded})
 
   // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
+  useDocumentTitle("Underreacted | Home");
 
   return (
     <>
