b3b07b41c1c9dab44714c45f43fd3a0279a06a66
Allan <kipropsallan@gmail.com>
2022-07-24 14:01:16 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <aside>
       <img src={logo} alt="blog logo" />
-      <p>A blog about learning React</p>
+      <p>My learning Journey with React</p>
     </aside>
   );
 }
